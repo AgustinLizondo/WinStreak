72927d05a4b243c5f7a317e834f6d3247aa9f83f
Agustín Lizondo <84605391+AgustinLizondo@users.noreply.github.com>
2021-08-23 16:07:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ let bPhoto = document.querySelectorAll('.bPhoto');
 let shadowContainer = document.querySelector('.shadowContainer');
 
 let moveLeft = ()=>{
-    shadowContainer.classList.add('moveLeft');
+    shadowContainer.classList.toggle('moveLeft');
 }
 let moveRight = ()=>{
-    shadowContainer.classList.add('moveRight');
+    shadowContainer.classList.toggle('moveRight');
 }
+let moveCenter = ()=>{
+    shadowContainer.classList.toggle('moveCenter')
+}
+
 
 for (let i of bPhoto){
     i.addEventListener('click', ()=>{
@@ -14,8 +18,23 @@ for (let i of bPhoto){
     })
     setInterval(() => {
         moveLeft();
-    }, 2500);
+    }, 5000);
     setInterval(() => {
         moveRight();
-    }, 7500);
+    }, 10000);
+    setInterval(() => {
+        moveCenter();
+    }, 15000);
 }
+
+// let moving = true;
+// while (moving){
+//     setInterval(() => {
+//         moveLeft();     
+//     }, 2500);
+//     setInterval(() => {
+//         moveRight();     
+//     }, 7500);
+//     setTimeout(() => {
+//         moving = false;    
+//     }, 50000); }
