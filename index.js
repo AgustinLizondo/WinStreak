ba65ebf62f9a4dba0a10329a459e692c427e83b4
Agustín Lizondo <84605391+AgustinLizondo@users.noreply.github.com>
2021-08-23 15:20:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 let bPhoto = document.querySelectorAll('.bPhoto');
+let shadowContainer = document.querySelector('.shadowContainer');
+
+let moveLeft = ()=>{
+    shadowContainer.classList.add('moveLeft');
+}
+let moveRight = ()=>{
+    shadowContainer.classList.add('moveRight');
+}
 
 for (let i of bPhoto){
     i.addEventListener('click', ()=>{
         window.location.assign('product.html');
     })
+    setInterval(() => {
+        moveLeft();
+    }, 2500);
+    setInterval(() => {
+        moveRight();
+    }, 7500);
 }
